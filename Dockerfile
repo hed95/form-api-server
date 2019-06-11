4f92d672a60c26de537497e3f37c62643444050a
Amin <amin@digitalpatterns.io>
2019-06-11 11:31:02 +0100

    Resolves: #2

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM digitalpatterns/node:latest
+
+WORKDIR /app
+
+RUN mkdir -p /app
+
+ADD . /app/
+
+RUN npm ci && npm run build-ts
+
+RUN chown -R node:node /app
+
+ENV NODE_ENV='production'
+
+USER 1000
+
+EXPOSE 8080
+
+ENTRYPOINT exec node dist/bootstrap.js
+
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ module.exports = {
             dialect: 'sqlite',
             storage: ':memory:',
         },
-        prod: {
+        production: {
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
